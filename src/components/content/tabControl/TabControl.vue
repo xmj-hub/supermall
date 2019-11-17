1ea65c17238c973bd20520e947dab47ca392cbfd
xmj <1147662815@qq.com>
2019-11-17 20:08:56 +0800

    1

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="tab-control">
+       
+        <div class="tab-control-item" v-for="(item,index) in titles"  :key="index" 
+        :class="{active:index===currentindex}" @click="indexitem(index)">
+           
+              <span>{{item}}</span>  
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'TabControl',
+    data(){
+        return{
+              currentindex:0
+        }
+     
+    },
+    methods:{
+        indexitem(index){
+            this.currentindex=index
+        }
+    },
+    props:{
+        titles:{
+            type:Array,
+            default(){
+                return []
+            }
+        },
+       
+    }
+}
+</script>
+
+<style scoped>
+    .tab-control{
+        display: flex;
+        background-color: #fff;
+    }
+    .tab-control-item{
+        flex:1;
+        text-align: center;
+        line-height: 40px;
+        font-size: 15px;
+    }
+    .active span{
+        padding: 5px;
+        color: var(--color-high-text);
+        border-bottom: 3px solid var(--color-tint)
+
+    }
+</style>
--- src/view/Home/Home.vue
+++ src/view/Home/Home.vue
@@ -10,44 +10,261 @@
      </a>
    </swiper-item>
    </swiper>
+   <div class="recommend" >
+     <div v-for="(item,index) in recommend" :key="index" class="recommend_item">
+            <a :href="item.link">
+       <img :src="item.image" alt="">
+       <p>{{item.title}}</p>
+     </a>
+     </div>
+   </div>
+
+     <div class="featureview">
+       <a href="https://act.mogujie.com/zzlx67">
+         <img src="~assets/img/home/recommend_bg.jpg" alt="">
+       </a>
+     </div>
+
+     <tab-control :titles="['流行','新款','精选']" class="tab-control"></tab-control>
+     <div>
+       <ul>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+         <li>111</li>
+       </ul>
+     </div>
+     
 </div>
 
 </template>
 
 <script>
-import NavBar from 'components/common/navbar/NavBar'
+
 import {getHomeMultidata} from "network/home";
-// import Swiper from 'components/common/swiper/Swiper'
-// import Swiper from 'components/common/swiper/SwiperItem'
+import {getHomeGoods} from "network/home";
 import {Swiper,SwiperItem} from 'components/common/swiper'
+import NavBar from 'components/common/navbar/NavBar'
+import TabControl from 'components/content/tabControl/TabControl'
   export default{
     name:'Home',
     data(){
        return{
         banners:null,
-        recommend:null
+        recommend:null,
+        goods:{
+          'pop':{page:0,list:[]},
+          'news':{page:0,list:[]},
+          'sell':{page:0,list:[]}
+        }
        }
     },
     components:{
       NavBar,
       Swiper,
-      SwiperItem
+      SwiperItem,
+      TabControl
     },
     created(){
       //1.请求多个数据
       getHomeMultidata().then(res=>{
         this.banners=res.data.banner.list;
-        this.recommend=res.data.recommend.list
-
+        this.recommend=res.data.recommend.list;
+        console.log(this.banners)
         
       })
+      // getHomeGoods('pop',1).then(res=>{
+      //   console.log(res)
+      // })
     }
   }
 </script>
 
 <style>
+#home{
+  padding-top: 44px;
+}
 .home-nav{
+  
   background-color: var(--color-tint);
   color:white;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 9;
+}
+.recommend{
+  display: flex;
+  height: 110;
+  border-bottom: 5px solid #eeefff;
+}
+.recommend .recommend_item{
+  flex: 1;
+  text-align: center;
+
+}
+.recommend .recommend_item a img{
+    margin-top: 10px;
+  width: 60px;
+  height: 60px;
+}
+.recommend .recommend_item a p{
+  font-size: 14px;
+  margin-top: 10px;
+  margin-bottom: 30px;
+}
+.featureview img{
+  width: 100%;
+}
+.tab-control{
+  position: sticky;
+  top: 44px
 }
 </style>
