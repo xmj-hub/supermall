c3cb98c7d3ec838fecf6cd3c62879d2e3e3a4d07
xmj-hub <1147662815@qq.com>
2019-12-31 17:31:24 +0800

    1

--- src/view/Profile/Profile.vue
+++ src/view/Profile/Profile.vue
@@ -1,9 +1,21 @@
 <template>
  <div class="profile">
    <div class="name">我的档案</div>
-   <div class="bg">111</div>  
-   <div class="bg">222</div>  
-   <div class="bg">333</div>  
+   
+   <div class="bg">查看订单           全部订单></div>  
+   <div class="bg">
+      <div>查看订单</div>
+      <div>我的优惠卷</div>
+      <div>我的收藏</div>
+      <!-- <div></div> -->
+      
+    </div>  
+   <div class="bg">
+     <div>我的等级</div>
+     <div>我的M币</div>
+     <div>我的优惠卷</div>
+   
+   </div>  
 
  </div>
 </template>
@@ -33,12 +45,12 @@
   color: white;
 }
 .bg{
-  border-radius: 30px;
+  /* border-radius: 30px; */
   background-color: #fff;
   
   width: 100%;
   height: 189px;
   margin-bottom: 10px;
-  text-align: center;
+  /* text-align: center; */
 }
 </style>
